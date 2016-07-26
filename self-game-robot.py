c7259f1f11b2e5a851d4af32752d44377f98cbba
Thelordofdream <574483817@qq.com>
2016-07-26 16:16:01 +0800

    version 2.0

--- self-game-robot.py
+++ self-game-robot.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import random
+import numpy as np
 
 # 吸 = 积累一颗子弹 0
 # Biu = 一级伤害,消耗一颗子弹 1
@@ -13,6 +14,7 @@ class player:
         self.Vtimes = 0
         self.Rounds = 0
         self.History = []
+        self.status = 1
 
 moves = ["挡挡", "挡", "吸", "Biu", "BiuBiu"]
 player1 = player()
@@ -20,29 +22,59 @@ player2 = player()
 player3 = player()
 players = [player1, player2, player3]
 each_round = [0, 0, 0]
-count = 0
+iteration = 0
+num = 10
+round_history = [[0] * 31 for i in range(3 * num)]
 
-while count < 1:
-    round_history1 = []
-    round_history2 = []
-    round_history3 = []
-    round_history = [round_history1, round_history2, round_history3]
-    for i in range(3):
-        if players[i].Bullet == 0:
-            each_round[i] = random.randint(-1, 0)
-        elif players[i].Bullet == 1:
-            each_round[i] = random.randint(-2, 1)
-        elif players[i].Bullet >= 2:
-            each_round[i] = random.randint(-2, 2)
-        round_history[i].append(each_round[i])
-    if i in range(3):
-        if each_round[i] == 0:
-            players[i].Bullet += 1
-        elif each_round[i] == 1 or each_round[i] == -2:
-            players[i].Bullet -= 1
-        elif each_round[i] == 2:
-            players[i].Bullet -= 2
-        print
+while iteration < num:
+    print "*** Iteration %d ***" % (iteration + 1)
+    count = 0
+    losers = 0
+    result = [0, 0, 0]
+    while True:
+        print "*** Round %d ***" % (count + 1)
+        Max = -2
+        # 出招
+        for i in range(3):
+            if players[i].Bullet == 0:
+                each_round[i] = random.randint(-1, 0)
+            elif players[i].Bullet == 1:
+                each_round[i] = random.randint(-2, 1)
+            elif players[i].Bullet >= 2:
+                each_round[i] = random.randint(-2, 2)
+            round_history[i + iteration * 3][count] = each_round[i]
+            if players[i].status == 1 and each_round[i] >= Max:
+                Max = each_round[i]
+            # if players[i].status == 1:
+            print "Player %d: %s" % ((i + 1), moves[each_round[i]+2])
+        # 结算子弹
+        for i in range(3):
+            if players[i].status == 1:
+                if each_round[i] == 0:
+                    players[i].Bullet += 1
+                elif each_round[i] == 1 or each_round[i] == -2:
+                    players[i].Bullet -= 1
+                elif each_round[i] == 2:
+                    players[i].Bullet -= 2
+                print "Player %d's bullet: %d" % ((i + 1), players[i].Bullet)
+        # 结算结果
+        for i in range(3):
+            if players[i].status == 1:
+                if (each_round[i] + Max) > 0 and each_round[i] != Max:
+                    print "Player %d loss" % (i + 1)
+                    players[i].status = 0
+                    losers += 1
+        if losers == 2:
+            for i in range(3):
+                if players[i].status == 1:
+                    print "Player %d win" % (i + 1)
+                    result[i] = 1
+                    print result
+                round_history[i+ iteration * 3][-1] = str(result[i])
+            break
+        count += 1
     for i in range(3):
         players[i].Bullet = 0
-    count += 1
+        players[i].status = 1
+    iteration += 1
+print np.array(round_history)
