0c80327c9b12ee2a5ac1a4750b302a0a6ca47011
Thelordofdream <574483817@qq.com>
2016-07-27 14:50:44 +0800

    version 6.0

--- Predict.py
+++ Predict.py
@@ -1,4 +1,11 @@
+# coding=utf-8
 from trees import *
+
+# 吸 = 积累一颗子弹 0
+# Biu = 一级伤害,消耗一颗子弹 1
+# 挡 = 能阻挡一级伤害,无消耗 -1
+# BiuBiu = 二级伤害,消耗两颗子弹 2
+# 挡挡 = 能阻挡一级和二级伤害,消耗一颗子弹 -2
 
 mytree = grabTree('Tree.txt')
 history = [0, -1, 0]
