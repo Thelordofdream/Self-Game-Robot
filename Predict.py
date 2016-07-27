8f6758dae0ffc29475ae57b958d86dee2fb2de6c
Thelordofdream <574483817@qq.com>
2016-07-27 15:04:16 +0800

    version 6.0

--- Predict.py
+++ Predict.py
@@ -9,5 +9,6 @@ from trees import *
 
 mytree = grabTree('Tree.txt')
 history = [0, -1, 0]
-rate =float(predict(history, mytree, 2, 1))
+round = 2
+rate =float(predict(history, mytree, round - 1, -2))
 print rate
